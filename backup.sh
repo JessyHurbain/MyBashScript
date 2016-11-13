0074a89cc88ca76ba81568886dc84c55d1c096f5
Jessy H <jessyhurbain@gmail.com>
2016-11-13 19:06:26 +0100

    Save list softwares installed and exclude the files with example.txt

--- backup.sh
+++ backup.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-DATE=$(date +%Hh%M-%d-%m-%y)
+#Format Hours / Minutes / Day / Month / Years
+DATE=$(date +%H-%M-%d-%m-%y)
 if [ -d "${DATE}" ]
 then
   echo "The directory ${DATE} already exists..."
   echo
 else
-      echo "Backup running ..."
+      #Save list softwares installed.
+      echo "Save softwares installed..."
+      dpkg --get-selections > /Destination/$DATE
+      sleep 3
+      echo "Save list softwares install succeeded"
+
+      echo "Backup running..."
       #Add custom directory source and destination
-      rsync -r --progress --delete /source /destination
+      # exclude The files assigned in the example.txt with  --exclude-from="/destination/example.txt"
+      rsync -r --progress --delete --exclude-from="/example.txt" /source /destination/$DATE
       echo "Backup succeeded"
 fi
