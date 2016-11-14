c6a9f3d15dc7bb3a408b557591cb3913f4447d50
JessyHurbain <Jessyhurbain@gmail.com>
2016-11-14 10:20:47 +0100

    add variable SOURCE DESTINATION

--- backup.sh
+++ backup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+
+SOURCE='/SOURCE'
+DESTINATION='/DESTINATION'
 #Format Hours / Minutes / Day / Month / Years
 DATE=$(date +%H-%M-%d-%m-%y)
 if [ -d "${DATE}" ]
@@ -9,13 +12,13 @@ then
 else
       #Save list softwares installed.
       echo "Save softwares installed..."
-      dpkg --get-selections > /Destination/$DATE
+      dpkg --get-selections > /$DESTINATION/$DATE
       sleep 3
       echo "Save list softwares install succeeded"
 
       echo "Backup running..."
       #Add custom directory source and destination
       # exclude The files assigned in the example.txt with  --exclude-from="/destination/example.txt"
-      rsync -r --progress --delete --exclude-from="/example.txt" /source /destination/$DATE
+      rsync -r --progress --delete --exclude-from="/example.txt" /$SOURCE /$DESTINATION/$DATE
       echo "Backup succeeded"
 fi
